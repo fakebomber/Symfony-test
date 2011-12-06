7c4e4e8df31132afce96a54ae592d0a311139db0
Ivan Tsigankov <fakebomber@gmail.com>
2011-12-06 18:41:03 +0200

    Doctrine added & files

--- src/FirstBundle/Controller/AdminController.php
+++ src/FirstBundle/Controller/AdminController.php
@@ -2,12 +2,40 @@
   namespace FirstBundle\Controller ;
   
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use FirstBundle\Entity\Product;
+  use Symfony\Component\HttpFoundation\Response;
   
   class AdminController extends Controller
   {
-    public function indexAction ()
+    public function indexAction()
     {
-      return $this->render('::base.html.twig');
+      return $this->render('::base.html.twig');      
+    }
+    
+    public function addAction() {
+      
+      $product = new Product();
+      $product->setName('A Foo Bar');
+      $product->setPrice('19.99');
+      $product->setDescription('Lorem ipsum dolor');
+      
+      $em = $this->getDoctrine()->getEntityManager();
+      $em->persist($product);
+      $em->flush();      
+      
+      return new Response('Created product id '.$product->getId());
+    }
+    
+    public function showAction($id) {
+      
+        $product = $this->getDoctrine()->getRepository('FirstBundle:Product')->find($id);
+      
+        if( !$product) {
+          throw $this->createNotFoundException( 'No product found for id '.$id );
+        }
+        else {
+          return new Response($product->getName()) ;
+        }
     }
     
     public function taskAction( $param )
